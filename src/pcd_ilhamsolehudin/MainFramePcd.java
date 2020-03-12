ca83a25e3dc9f6d99ef75fa33ebe1ef641ecdad5
lab1a <lab1a@TUJUHBELAS>
2020-03-12 12:55:39 +0700

    1.gray 2. instan cahaya

--- src/pcd_ilhamsolehudin/MainFramePcd.java
+++ src/pcd_ilhamsolehudin/MainFramePcd.java
@@ -1,3 +1,5 @@
+package pcd_ilhamsolehudin;
+
 
 import java.awt.Color;
 import java.awt.Dimension;
@@ -30,12 +32,14 @@ import pcd_ilhamsolehudin.ImagePanel;
 public class MainFramePcd extends JFrame implements MouseListener, MouseMotionListener, MouseWheelListener {
 
     private JButton btnLoad;
+    private JButton btnConvert;
     private ImagePanel mPanel;
+    private ImagePanel mHasilPanel;
     private JFileChooser mChooser;
     private int maxSize = 10;
     private int minSize = 1;
     private int currentSize = 5;
-
+    private int nilaiCayaha = 10;
     public static void main(String[] args) {
         // TODO code application logic here
         new MainFramePcd();
@@ -56,26 +60,89 @@ public class MainFramePcd extends JFrame implements MouseListener, MouseMotionLi
             }
         }
     };
+    
+        private final ActionListener btnConnvertListner = new ActionListener() {
+
+        @Override
+        public void actionPerformed(ActionEvent ae) {
+            if(mPanel.getImage() !=null){
+                BufferedImage image =new BufferedImage(mPanel.getImage().getWidth(), 
+                        mPanel.getImage().getHeight(), mPanel.getImage().getType());
+                for( int y  = 0 ; y<mPanel.getImage().getHeight();y++){
+                    for(int x = 0 ; x<mPanel.getImage().getWidth();x++){
+                        int rgb = mPanel.getImage().getRGB(x, y);
+                        Color c = new Color(rgb);
+                        int gray = c.getRed()+c.getGreen()+c.getBlue() ;
+                        gray = gray/3;
+                        gray  =  gray;
+                        gray = gray>255?255:gray;
+                 
+                    
+                        Color mask = new Color(gray,gray,gray);
+                        image.setRGB(x, y, mask.getRGB());
+                      
+                        
+                }
+            }
+                  mHasilPanel.setImage(image);
+                  mHasilPanel.repaint();
+            }
+        }
+    };
+        
+        BufferedImage instensCahaya(BufferedImage image){
+              BufferedImage hasil =new BufferedImage(mPanel.getImage().getWidth(), 
+                        mPanel.getImage().getHeight(), mPanel.getImage().getType());
+                for( int y  = 0 ; y<mPanel.getImage().getHeight();y++){
+                    for(int x = 0 ; x<mPanel.getImage().getWidth();x++){
+                        int rgb = mPanel.getImage().getRGB(x, y);
+                        Color c = new Color(rgb);
+                        int gray = c.getRed()+c.getGreen()+c.getBlue() ;
+                        gray = gray/3;
+                        gray  =  gray;
+                        gray = gray>255?255:gray;
+                 
+                    
+                        Color mask = new Color(gray,gray,gray);
+                        image.setRGB(x, y, mask.getRGB());
+                      
+                        
+                }
+            }
+            return hasil;
+        }
 
     public MainFramePcd() {
         super("GUI IMage");
-        this.setSize(500, 500);
+        this.setSize(new Dimension(800,500));    
         this.setLayout(new FlowLayout());
 
         //initial 
         btnLoad = new JButton("Load Image");
         this.add(btnLoad);
         btnLoad.addActionListener(btnLoadOnclick);
+        
+        btnConvert = new JButton("Convert Image");
+        this.add(btnConvert);
+        btnConvert.addActionListener(btnConnvertListner);
 
         mChooser = new JFileChooser();
 
         mPanel = new ImagePanel();
-        mPanel.setPreferredSize(new Dimension(400, 400));
-        mPanel.setBackground(Color.white);
+        mPanel.setPreferredSize(new Dimension(800, 600));
+        mPanel.setBackground(Color.WHITE);
+        
         this.add(mPanel);
         mPanel.addMouseListener(this);
         mPanel.addMouseMotionListener(this);
         mPanel.addMouseWheelListener(this);
+        
+        
+        mHasilPanel = new ImagePanel();
+        mHasilPanel.setPreferredSize(new Dimension(800, 600));
+        mHasilPanel.setBackground(Color.WHITE);
+        
+        this.add(mHasilPanel);
 
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
